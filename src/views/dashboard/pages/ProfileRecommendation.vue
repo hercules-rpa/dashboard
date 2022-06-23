f83b8f7128573f3c431d1fcf81de724072f89aa8
educanvoas93 <educanovas93@uoc.edu>
2022-06-23 08:53:59 +0200

    Estrellas

--- src/views/dashboard/pages/ProfileRecommendation.vue
+++ src/views/dashboard/pages/ProfileRecommendation.vue
@@ -1,107 +1,121 @@
 <template>
-  <v-card>
-    <v-toolbar
-      color="primary"
-      dark
-      flat
+  <v-container
+    id="new-process"
+    fluid
+    tag="section"
+  >
+    <base-material-card
+      icon="mdi-clipboard-text"
+      title="Profile Recomendation"
+      class="px-5 py-3"
     >
-      <v-icon>mdi-account-box</v-icon>
-      <v-toolbar-title>Perfil de Recomendación</v-toolbar-title>
-    </v-toolbar>
-    <v-row>
-      <v-col
-        cols="12"
-        md="12"
-      >
-        <v-card-text>
-          <v-treeview
-            v-model="tree"
-            :items="items"
-            selected-color="indigo"
-            return-object
-            item-key="id"
-          >
-            <template v-slot:label="{item}">
-              <div class="row">
-                <div class="columnAreas">
-                  {{ item.name }}
-                </div>
-                <div class="columnRating">
-                  <input
-                    v-if="item.id != 0"
-                    id="checkbox"
-                    v-model="rating"
-                    :value="item"
-                    type="checkbox"
-                    style="width:100%;align:right;margin: auto;"
-                  >
-                </div>
-              </div>
-            </template>
-          </v-treeview>
-        </v-card-text>
-      </v-col>
-      <v-divider vertical />
-
-      <v-col
-        cols="12"
-        md="4"
-      >
-        <v-card-text>
-          <div
-            v-if="tree.length === 0"
-            key="title"
-            class="text-h4 font-weight-light pa-4 text-center"
-          >
-            <span> {{ rating }} </span>
-          </div>
-
-          <v-scroll-x-transition
-            group
-            hide-on-leave
-          >
-            <v-chip
-              v-for="(selection, i) in tree"
-              :key="i"
-              color="grey"
-              dark
-              small
-              class="ma-1"
+      <v-row>
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-card-text>
+            <v-treeview
+              v-model="tree"
+              rounded
+              hoverable
+              :items="items"
+              selection-type="leaf"
+              selected-color="indigo"
+              return-object
+              item-key="id"
             >
-              <v-icon
-                left
-                small
-              >
-                mdi-beer
-              </v-icon>
-              {{ selection.name }}
-            </v-chip>
-          </v-scroll-x-transition>
-        </v-card-text>
-      </v-col>
-    </v-row>
+              <template v-slot:label="{item}">
+                <div class="row">
+                  <div class="columnAreas">
+                    {{ item.name }}
+                  </div>
+                  <div class="columnRating">
+                    <v-rating
+                      v-if="item.id != 0"
+                      v-model="item.rating"
+                      empty-icon="mdi-star-outline"
+                      full-icon="mdi-star"
+                      half-icon="mdi-star-half-full"
+                      hover
+                      background-color="grey"
+                      color="orange"
+                      yellow
+                      length="5"
+                      size="20"
 
-    <v-divider />
-    <v-card-actions>
-      <v-btn
-        text
-        @click="tree = [], rationg = []"
-      >
-        Reset
-      </v-btn>
-      <v-spacer />
-      <v-btn
-        class="white--text"
-        color="green darken-1"
-        depressed
-      >
-        Save
-        <v-icon right>
-          mdi-content-save
-        </v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+                      value="0"
+                      @input="selectItem(item)"
+                    />
+                  </div>
+                </div>
+              </template>
+            </v-treeview>
+          </v-card-text>
+        </v-col>
+        <v-divider vertical />
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-card-text>
+            <div
+              v-if="tree.length === 0"
+              key="title"
+              class="text-h4 font-weight-light pa-4 text-center"
+            >
+              <span>Rating : {{ rating }} </span>
+              <span>Tree : {{ tree }}</span>
+            </div>
+            <div><span>Tree : {{ tree }}</span></div>
+            <v-scroll-x-transition
+              group
+              hide-on-leave
+            >
+              <v-chip
+                v-for="(selection, i) in tree"
+                :key="i"
+                color="grey"
+                dark
+                small
+                class="ma-1"
+              >
+                <v-icon
+                  left
+                  small
+                >
+                  mdi-beer
+                </v-icon>
+                {{ selection.name }}
+              </v-chip>
+            </v-scroll-x-transition>
+          </v-card-text>
+        </v-col>
+      </v-row>
+
+      <v-divider />
+      <v-card-actions>
+        <v-btn
+          text
+          @click="tree = [], rating = [], itemsSelected = []"
+        >
+          Reset
+        </v-btn>
+        <v-spacer />
+        <v-btn
+          class="white--text"
+          color="green darken-1"
+          depressed
+        >
+          Save
+          <v-icon right>
+            mdi-content-save
+          </v-icon>
+        </v-btn>
+      </v-card-actions>
+    </base-material-card>
+  </v-container>
 </template>
 
 <script>
@@ -125,6 +139,7 @@
         areastematicas: [],
         tree: [],
         rating: [],
+        itemsSelected: new Map(),
       }
     },
     computed: {
@@ -153,6 +168,46 @@
     },
 
     methods: {
+      fillChildrenRecursive (item) {
+        // console.log('recursive')
+        if (item.children.length > 0) {
+          item.children.forEach(element => {
+            element.rating = item.rating
+            this.fillChildrenRecursive(item)
+          })
+        }
+      },
+      isInAnyChildren (item) {
+        this.itemsSelected.forEach(element => {
+          return this.isInItem(element, item.id)
+        })
+      },
+      isInItem (item, id) {
+        if (item.children.length > 0) {
+          item.children.forEach(element => {
+            if (id === element.id) {
+              return true
+            } else {
+              this.isInItem(element, id)
+            }
+          })
+        } else {
+          return false
+        }
+      },
+      selectItem (item) {
+        if (!this.isInAnyChildren(item)) {
+          this.itemsSelected.set(item.id, item)
+          // item.select()
+
+          this.fillChildrenRecursive(item)
+
+          // this.tree.push(this.item)
+          console.log('ITEM :', item)
+          console.log(this.itemsSelected)
+        }
+      },
+
       getChildren (areatematica) {
         const areasHijos = []
         if (areatematica[areatematica.id].length > 0) {
