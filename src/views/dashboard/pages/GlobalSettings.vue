5658167c799fbea6115043d73bc7d7da982aed8f
educanvoas93 <educanovas93@uoc.edu>
2022-09-16 07:49:17 +0200

    Globalsettings

--- src/views/dashboard/pages/GlobalSettings.vue
+++ src/views/dashboard/pages/GlobalSettings.vue
@@ -53,8 +53,22 @@
                   :validation="[['matches', /^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$/],['required']]"
                 />
                 <FormulateInput
+                  type="text"
+                  name="DATABASE_IP"
+                  label="DATABASE IP"
+                  default="sgi.um.es"
+                  :validation="[['matches', /(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/],['required']]"
+                />
+                <FormulateInput
+                  type="text"
+                  name="DATABASE_PORT"
+                  label="DATABASE PORT"
+                  default="5432"
+                  :validation="[['matches', /^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$/],['required']]"
+                />
+                <FormulateInput
                   type="submit"
-                  label="Ejecutar"
+                  label="Guardar"
                 />
               </FormulateForm>
 
